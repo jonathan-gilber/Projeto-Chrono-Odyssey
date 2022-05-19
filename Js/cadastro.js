63f2a2f188df9c89a02d4b90c30a74ae8f0ed23e
Palenkova <jonathan.gilber@sptech.school>
2022-05-19 10:52:12 -0300

    Reversão de galeria de midia para html e css (sem bootstrap)

--- Js/cadastro.js
+++ Js/cadastro.js
@@ -1,124 +1,122 @@
 function validCadastro() {
-
   /* Verifica se há algum input com valor vazio e ajusta o layout para o texto caber na tela caso houver */
-  if (input_nome.value == '' || input_tel.value == '' || input_senha.value == '' || input_sobrenome.value == '' || input_email.value == '' || input_repetir_senha.value == '') {
-    span_validacao.innerHTML = 'Por favor preencha todos os campos';
+  if (
+    input_nome.value == "" ||
+    input_tel.value == "" ||
+    input_senha.value == "" ||
+    input_sobrenome.value == "" ||
+    input_email.value == "" ||
+    input_repetir_senha.value == ""
+  ) {
+    span_validacao.innerHTML = "Por favor preencha todos os campos";
     idcontainer2.style.marginTop = "-25px";
-    texto_cadastro.style.marginBottom = "30px"
+    texto_cadastro.style.marginBottom = "30px";
 
     /* marca o campo sobrenome se estiver vazio */
-    if (input_nome.value !== '') {
-      input_nome.style.border = "none"
+    if (input_nome.value !== "") {
+      input_nome.style.border = "none";
     } else {
-      input_nome.style.border = "thin solid #FF0000"
+      input_nome.style.border = "thin solid #FF0000";
     }
 
     /* marca o campo email se estiver vazio */
-    if (input_email.value !== '') {
-      input_email.style.border = "none"
+    if (input_email.value !== "") {
+      input_email.style.border = "none";
     } else {
-      input_email.style.border = "thin solid #FF0000"
+      input_email.style.border = "thin solid #FF0000";
     }
 
     /* marca o campo nome/razão social se estiver vazio */
-    if (input_sobrenome.value !== '') {
-      input_sobrenome.style.border = "none"
+    if (input_sobrenome.value !== "") {
+      input_sobrenome.style.border = "none";
     } else {
-      input_sobrenome.style.border = "thin solid #FF0000"
+      input_sobrenome.style.border = "thin solid #FF0000";
     }
 
     /* marca o campo senha se estiver vazio */
-    if (input_senha.value !== '') {
-      input_senha.style.border = "none"
+    if (input_senha.value !== "") {
+      input_senha.style.border = "none";
     } else {
-      input_senha.style.border = "thin solid #FF0000"
+      input_senha.style.border = "thin solid #FF0000";
     }
 
     /* marca o campo repetir senha se estiver vazio */
-    if (input_repetir_senha.value !== '') {
-      input_repetir_senha.style.border = "none"
+    if (input_repetir_senha.value !== "") {
+      input_repetir_senha.style.border = "none";
     } else {
-      input_repetir_senha.style.border = "thin solid #FF0000"
+      input_repetir_senha.style.border = "thin solid #FF0000";
     }
 
     /* marca o campo telefone se estiver vazio */
-    if (input_tel.value !== '') {
-      input_tel.style.border = "none"
+    if (input_tel.value !== "") {
+      input_tel.style.border = "none";
     } else {
-      input_tel.style.border = "thin solid #FF0000"
+      input_tel.style.border = "thin solid #FF0000";
     }
-  }
-
-  else {
+  } else {
     /* tira as marcações das inputs já preenchidas */
-    input_tel.style.border = "none"
-    input_senha.style.border = "none"
-    input_repetir_senha.style.border = "none"
-    input_nome.style.border = "none"
-    input_email.style.border = "none"
-    input_sobrenome.style.border = "none"
+    input_tel.style.border = "none";
+    input_senha.style.border = "none";
+    input_repetir_senha.style.border = "none";
+    input_nome.style.border = "none";
+    input_email.style.border = "none";
+    input_sobrenome.style.border = "none";
 
     /* Valida se o email possui "@" */
     if (input_email.value.indexOf("@") == -1) {
-      input_email.style.border = "thin solid #FF0000"
-      span_validacao.innerHTML = 'Email inválido, deve conter "@"'
-    }
+      input_email.style.border = "thin solid #FF0000";
+      span_validacao.innerHTML = 'Email inválido, deve conter "@"';
+    } else if (input_email.value.length < 10) {
 
     /* Valida se o email possui mais de 10 caracteres */
-    else if (input_email.value.length < 10) {
-      input_email.style.border = "thin solid #FF0000"
-      span_validacao.innerHTML = 'Email inválido, deve conter no mínimo 10 digitos'
-    }
+      input_email.style.border = "thin solid #FF0000";
+      span_validacao.innerHTML =
+        "Email inválido, deve conter no mínimo 10 digitos";
+    } else if (input_nome.value.length < 6) {
 
     /* Valida se o nome possui mais de 6 caracteres */
-    else if (input_nome.value.length < 6) {
-      input_nome.style.border = "thin solid #FF0000"
-      span_validacao.innerHTML = 'Nome deve conter no mínimo 6 digitos'
-    }
+      input_nome.style.border = "thin solid #FF0000";
+      span_validacao.innerHTML = "Nome deve conter no mínimo 6 digitos";
+    } else if (input_sobrenome.value.length < 3) {
 
     /* Valida se a input sobrenome possui mais de 3 caracteres */
-    else if (input_sobrenome.value.length < 3) {
-      input_sobrenome.style.border = "thin solid #FF0000"
-      span_validacao.innerHTML = 'Sobrenome deve conter no mínimo 3 digitos'
-      }
+      input_sobrenome.style.border = "thin solid #FF0000";
+      span_validacao.innerHTML = "Sobrenome deve conter no mínimo 3 digitos";
+    } else if (input_tel.value.length < 8) {
 
     /* Valida se o número de telefone possui ao menos 8 digitos */
-    else if (input_tel.value.length < 8) {
-    input_tel.style.border = "thin solid #FF0000"
-    span_validacao.innerHTML = 'Telefone deve conter no mínimo 8 digitos'
-    }
+      input_tel.style.border = "thin solid #FF0000";
+      span_validacao.innerHTML = "Telefone deve conter no mínimo 8 digitos";
+    } else if (input_senha.value.length < 8) {
 
     /* Valida se a senha tem mais de 8 caracteres */
-    else if (input_senha.value.length < 8) {
-      input_senha.style.border = "thin solid #FF0000"
-      span_validacao.innerHTML = 'Senha deve conter no mínimo 8 digitos'
-    }
+      input_senha.style.border = "thin solid #FF0000";
+      span_validacao.innerHTML = "Senha deve conter no mínimo 8 digitos";
+    } else if (input_senha.value !== input_repetir_senha.value) {
 
     /* Valida se as inputs de senha e confirmar senha são iguais */
-    else if (input_senha.value !== input_repetir_senha.value) {
-      input_senha.style.border = "thin solid #FF0000"
-      input_repetir_senha.style.border = "thin solid #FF0000"
-      span_validacao.innerHTML = 'As senhas não coincidem'
-    }
+      input_senha.style.border = "thin solid #FF0000";
+      input_repetir_senha.style.border = "thin solid #FF0000";
+      span_validacao.innerHTML = "As senhas não coincidem";
+    } else if (chk_termos.checked == false) {
 
     /* Valida se o checkbox de termos de uso foi aceito */
-    else if (chk_termos.checked == false) {
-      span_validacao.innerHTML = 'Você deve concordar com os nossos termos de uso antes de prosseguir'
-      chk_termos.style.outline = "1px solid red"
-      chk_termos.style.outlineOffset = "-1px"
-    }
-    
+      span_validacao.innerHTML =
+        "Você deve concordar com os nossos termos de uso antes de prosseguir";
+      chk_termos.style.outline = "1px solid red";
+      chk_termos.style.outlineOffset = "-1px";
+    } else {
+
     /* Confirma o cadastro e abre a tela de monitoramento */
-    else {
-      input_tel.style.border = "none"
-      input_senha.style.border = "none"
-      input_repetir_senha.style.border = "none"
-      input_nome.style.border = "none"
-      input_email.style.border = "none"
-      input_sobrenome.style.border = "none"
-      
-      alert('Cadastro efetuado com sucesso')
-      window.location.href = 'index.html'
+      input_tel.style.border = "none";
+      input_senha.style.border = "none";
+      input_repetir_senha.style.border = "none";
+      input_nome.style.border = "none";
+      input_email.style.border = "none";
+      input_sobrenome.style.border = "none";
+
+      alert("Cadastro efetuado com sucesso");
+      window.location.href = "index.html";
     }
   }
 }
--- Js/index.js
+++ Js/index.js
@@ -1,37 +1,36 @@
 // tocar música com o botão de play
 function tocar() {
- var audio = new Audio("./Assets/Index_Audio.mp3");
+  var audio = new Audio("./Assets/Index_Audio.mp3");
   audio.play();
-  }
+}
 
-  // Âncora até determinado elemento da página
-  function saibaMais() {
-    window.location.href= "#ancora_conteudo"
-  }
+// Âncora até determinado elemento da página
+function saibaMais() {
+  window.location.href = "#ancora_conteudo";
+}
 
-  function galeria_midia() {
-    window.location.href= "#ancora_carrossel"
-  }
+function galeria_midia() {
+  window.location.href = "#ancora_galeria";
+}
 
-  // Define a data que queremos alcançar
-  var countDownDate = new Date("Jul 5, 2022 15:37:25").getTime();
-  
-  // atualiza o contador a cada 1 segundo
-  var x = setInterval(function() {
-  
-    // Define o dia e hora de hoje
-    var now = new Date().getTime();
-      
-    // descobre a distância entre agora e a data que buscamos
-    var distance = countDownDate - now;
-      
-    // cálculos para dias, minutos e horas
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      
-    // output do valor na div 'demo'
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-  }, 1000);
+// Define a data que queremos alcançar
+var countDownDate = new Date("Jul 5, 2022 15:37:25").getTime();
+
+// atualiza o contador a cada 1 segundo
+var x = setInterval(function () {
+  // Define o dia e hora de hoje
+  var now = new Date().getTime();
+
+  // descobre a distância entre agora e a data que buscamos
+  var distance = countDownDate - now;
+
+  // cálculos para dias, minutos e horas
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // output do valor na div 'demo'
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+}, 1000);
